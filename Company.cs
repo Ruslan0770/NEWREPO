24d678d9c7de2132a768b90721265f1ba8e71926
Ruslan0770 <ruslania@code.edu.az>
2022-04-18 14:44:01 -0700

    newtask

---
+++ Company.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework18042022.Models
+{
+    class Company
+    {
+        private string _name;
+        public int Limit { get; set; }
+        private Employee[] _avg;
+
+        public Company()
+        {
+            _avg = new Employee[0];
+        }
+
+        public void AddEmployee(Employee avg)
+        {
+            if (_avg.Length < Limit)
+            {
+                Array.Resize(ref _avg, _avg.Length + 1);
+                _avg[_avg.Length - 1] = avg;
+                return;
+            }
+            Console.WriteLine("yER DOLUB");
+        }
+        public Employee[] GetEmployees() => _avg;
+        public string Name {
+            get => _name;
+            set
+            {
+                bool check = true;
+                while (check)
+                {
+                        bool checksymbol = false;
+                        bool checkdigit = false;
+                        bool checkspacebar = false;
+                        bool checkupper = false;
+                    if (char.IsUpper(value[0]))
+                    {
+                        checkupper = true;
+                        foreach (char item in value)
+                        {
+                            if (char.IsDigit(item)) 
+                            {
+                                checkdigit = true;
+                            }
+                           else if (char.IsSymbol(item))
+                            {
+                                checksymbol = true;
+                            }
+                            else if (char.IsWhiteSpace(item))
+                            {
+                                checkspacebar = true;
+                            }
+                            if (checksymbol == true && checkdigit && checkspacebar)
+                            {
+                                _name = value;
+                                check = false;
+                                break;
+                            }
+                            if (checkupper == false || checkdigit == false || checksymbol == false || checkspacebar == false)
+                            {
+                                Console.WriteLine("Adi duzgun daxil et");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework18042022.Models
+{
+    class Employee
+    {
+        public static int id = 1;
+        public string Fullname { get; set; }
+        public int Salary { get; set; }
+        public string Position { get; set; }
+        public int No;
+        public Employee(string fullname,int salary,string position)
+        {
+            No = id;
+            id++;
+            Fullname = fullname;
+            Salary = salary;
+            Position = position;
+            
+        }
+    }
+}
